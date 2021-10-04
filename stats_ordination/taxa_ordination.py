a4994b2b0d3c25b226932180afd22671e8ed9776
catarinaloureiro <catarinacarolina13@gmail.com>
2021-10-04 14:43:57 +0200

    update samples and labels

--- stats_ordination/taxa_ordination.py
+++ stats_ordination/taxa_ordination.py
@@ -36,12 +36,9 @@ bc_matrix_path = cmds.matrix
 pcoa_path = cmds.out_pcoa
 perm_path = cmds.out_perm
 
-
-sample_n = ['Aply16', 'Aply21', 'Aply22', 'Aply23', 'Cr15', 'Cr50', 'Cr90', 'Dys1.1', 'Dys1.2', 'Dys2.1', 'Pf10', 'Pf11', 'Pf12', 'Pf4', 'Pf5', 'Pf6', 'Pf7', 'Pf8', 'Pf9','gb1', 'gb2_2', 'gb3_2', 'gb4_2', 'gb5_2', 'gb6', 'gb7', 'gb8_2', 'gb9', 'gb10','gb126', 'gb278', 'gb305', 'gb1_f', 'gb2_f', 'gb3_f', 'gb5_6_f', 'gb9_f', 'gb10_f', 'sw_7', 'sw_8', 'sw_9']
-
-
 bc_df = pd.read_csv(bc_matrix_path, sep='\t', index_col=0)
 meta_df = pd.read_excel(meta_path, index_col=0)
+sample_n = meta_df['True_sample']
 
 BC_dist = skbio.stats.distance.DistanceMatrix(bc_df, ids=sample_n)
 
@@ -56,14 +53,12 @@ bc_pcoa = skbio.stats.ordination.pcoa(BC_dist)
 
 bc_pcoa_df =bc_pcoa.samples[['PC1','PC2']]
 
-colours = ["#ffc410","#ffc411","#ffc412","#ffc413","#dc2a3d","#dc2a3d","#dc2a3d","#eb4ad0","#eb4ad1","#eb4ad2","#8a4bc9","#8a4bc10","#8a4bc11","#8a4bc12","#8a4bc13","#8a4bc14","#8a4bc15","#8a4bc16","#8a4bc17","#cedbde","#cedbde",  "#cedbde","#cedbde","#cedbde","#cedbde","#cedbde","#cedbde","#cedbde","#cedbde","#b0bcbf","#b0bcbf","#b0bcbf","#32a1ab","#32a1ab","#32a1ab","#32a1ab","#32a1ab","#32a1ab","#14cdde","#14cdde","#14cdde"]
-
-labels = ["Aplysina aerophoba","Aplysina aerophoba","Aplysina aerophoba","Aplysina aerophoba","Crambe crambe","Crambe crambe","Crambe crambe","Dysidea avara","Dysidea avara","Dysidea avara","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Petrocia ficiformis","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Nor","Geodia barretti Can","Geodia barretti Can","Geodia barretti Can","Seawater Atl","Seawater Atl","Seawater Atl","Seawater Atl","Seawater Atl","Seawater Atl","Seawater Med","Seawater Med","Seawater Med"]
+labels = meta_df['Species extra']
 
 bc_pcoa_df.insert(2,'Label', labels)
 
 sns.set_style("white")
-sns.scatterplot(data=bc_pcoa_df, x='PC1', y='PC2', hue='Label', palette=["#ffc410", "#c70e22", "#e86dd3", "#8a4bc9","#8d9091","#525454","#32a1ab","#14cdde"], alpha=0.75)
+sns.scatterplot(data=bc_pcoa_df, x='PC1', y='PC2', hue='Label', palette=["#ffc410","#8a4bc9","#8d9091","#525454","#32a1ab","#14cdde"], alpha=0.85, s=100)
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 # # add label to points on the plot
